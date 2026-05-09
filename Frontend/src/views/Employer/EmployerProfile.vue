0d893d2350c443d33dac9dfa2520aa8e092854a2
Huy Vui <tranhuyvui714@gmail.com>
2026-05-10 02:30:43 +0700

    fix: employer profile

--- Frontend/src/views/Employer/EmployerProfile.vue
+++ Frontend/src/views/Employer/EmployerProfile.vue
@@ -6,7 +6,6 @@ import Loading from '../../components/Loading.vue';
 import Notify from '../../components/Notify.vue';
 import type { ICompanyDetailResponse, IUpdateCompany } from '../../types/company';
 import { useCompanyStore } from '../../stores/company';
-import router from '../../router';
 
 const useCompany = useCompanyStore();
 
