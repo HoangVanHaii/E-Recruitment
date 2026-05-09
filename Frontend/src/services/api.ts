d6401559d6fd93031c29e04b629523910531113d
Hoàng Văn Hải <hoanggvanhaii0705@gmail.com>
2026-05-09 23:38:03 +0700

    upd: update baseUrl from api

--- Frontend/src/services/api.ts
+++ Frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { disconnectSocket } from "./socket";
 const api = axios.create({
-    baseURL: import.meta.env.REACT_APP_API_BASE_URL || 'https://jobportal-1-l1my.onrender.com/api',
+    baseURL: import.meta.env.REACT_APP_API_BASE_URL || 'https://jobportal-rs7w.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
