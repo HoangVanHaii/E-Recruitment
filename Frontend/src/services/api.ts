c2375828d91eb1ea143c9fd163c75c4c7bff0df8
Huy Vui <tranhuyvui714@gmail.com>
2026-05-10 03:27:26 +0700

    fix: api service FE

--- Frontend/src/services/api.ts
+++ Frontend/src/services/api.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
 import { disconnectSocket } from "./socket";
 const api = axios.create({
-    baseURL: import.meta.env.REACT_APP_API_BASE_URL || 'http://localhost:3000/api',
+    baseURL: import.meta.env.REACT_APP_API_BASE_URL || 'https://jobportal-rs7w.onrender.com/api',
 });
-//https://jobportal-rs7w.onrender.com/api
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('accessToken');
     if (token) {
