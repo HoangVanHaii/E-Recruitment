8fbdbb0b58776d84ea4e9e58b35a856a3a0c7435
Huy Vui <tranhuyvui714@gmail.com>
2026-05-10 02:01:00 +0700

    fix: Update limit page HomeView

--- Frontend/src/views/HomeView.vue
+++ Frontend/src/views/HomeView.vue
@@ -36,7 +36,7 @@ const currentKeyword = ref('');
 
 const filterState = reactive({
     page: 1,
-    limit: 9,
+    limit: 18,
     categoryId: undefined as number | undefined,
     location: '',
     minSalary: undefined as number | undefined,
